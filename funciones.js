5d1a4826eb283981ea56a33801dd6f8569df66cf
FedericoD3 <90582067+FedericoD3@users.noreply.github.com>
2022-09-26 20:51:41 -0400

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -3,9 +3,10 @@ let opcionesMapa = {
   zoom:16
 }
 
-let mapa = new L.map("mapa" , opcionesMapa);
-let capa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-mapa.addLayer(capa);
+let objMapa = new L.map('divMapa' , opcionesMapa);
 
-let marca = new L.Marker([10.2305,-67.3237]);
-marca.addTo(mapa);
+let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+objMapa.addLayer(objCapa);
+
+let objMarca = new L.Marker([10.2305,-67.3237]);
+objMarca.addTo(objMapa);
