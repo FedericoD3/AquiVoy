c8df242e78bb24aedebdca47460627fbc0c5e298
FedericoD3 <fvduran@gmail.com>
2022-09-27 15:58:36 -0400

    x

--- funciones.js
+++ funciones.js
@@ -8,7 +8,17 @@ let objMapa = new L.map('divMapa' , opcionesMapa);
 let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 objMapa.addLayer(objCapa);
 
+/*
 let objMarca = new L.Marker([10.228,-67.3237])
+*/
+
+/*  https://9to5answer.com/how-to-make-markers-in-leaflet-blinking  */
+let objMarca = new L.Marker([10.228,-67.3237], {
+  icon: L.icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.0.3/dist/images/marker-icon.png',
+    className: 'blinking'
+  })
+})
 
 objMarca.addTo(objMapa);
 /*
