fd90feab295457e94f58c46070a7fb9717fd57b0
FedericoD3 <fvduran@gmail.com>
2022-09-27 14:48:19 -0400

    index corregido y con imagenes

--- funciones.js
+++ funciones.js
@@ -1,5 +1,5 @@
 let opcionesMapa = {
-  center:[10.230,-67.324],
+  center:[10.228,-67.324],
   zoom:16
 }
 
@@ -8,5 +8,5 @@ let objMapa = new L.map('divMapa' , opcionesMapa);
 let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 objMapa.addLayer(objCapa);
 
-let objMarca = new L.Marker([10.2305,-67.3237]);
+let objMarca = new L.Marker([10.232,-67.3237]);
 objMarca.addTo(objMapa);
