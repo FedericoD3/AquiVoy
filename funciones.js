36ddedb9d4f445d3f07d1e3e63656125dc5d8aa5
FedericoD3 <fvduran@gmail.com>
2022-09-27 20:38:37 -0400

    Anotada logica de intermitencia del marker

--- funciones.js
+++ funciones.js
@@ -8,24 +8,22 @@ let objMapa = new L.map('divMapa' , opcionesMapa);
 let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 objMapa.addLayer(objCapa);
 
-/*
 let objMarca = new L.Marker([10.228,-67.3237])
-*/
-
-/*  https://9to5answer.com/how-to-make-markers-in-leaflet-blinking  */
-let objMarca = new L.Marker([10.228,-67.3237], {
-  icon: L.icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.0.3/dist/images/marker-icon.png',
-    className: 'blinking'
-  })
-})
 
 objMarca.addTo(objMapa);
+
 /*
-L.marker([10.232,-67.3237], {
-  icon: L.icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.0.3/dist/images/marker-icon.png',
-    className: 'destello'
-  })
-}).addTo(map);
+objMarca. 
+
+setInterval(function () {AlternarMarca()}, 1000);	// Actualizar el mapa cada pocos segundos
+
+*/
+
+/*
+variable opacidad
+cada 500mS
+  asignar opacidad = abs(opacidad - 1)
+  objMarca.setOpacity(opacidad)
+  Leer coordenadas nuevas de dweet.io
+  objMarca.setLatLng([lat,lon])
 */
