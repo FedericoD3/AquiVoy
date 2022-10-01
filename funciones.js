dd999524cc27495b774eddeabb9b64b2bbf43f78
FedericoD3 <fvduran@gmail.com>
2022-10-01 17:31:40 -0400

    Determinación de las coordenadas LV-LT

--- funciones.js
+++ funciones.js
@@ -1,9 +1,14 @@
 function mostrarMapa()
 {
 
+/*
 let opcionesMapa = {
-/*  center:[10.230,-67.324],
-  zoom:15 */
+  center:[10.230,-67.324],
+  zoom:15 
+}
+*/
+
+let opcionesMapa = {
   center:[10.235,-67.25],
   zoom:13
 }
