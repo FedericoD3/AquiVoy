2354d0f73fbb40d3cd24d474f55cceb8d0b3e46b
FedericoD3 <fvduran@gmail.com>
2022-10-01 19:58:18 -0400

    Revert "Determinacion de las coordenadas LV-LT"
    This reverts commit ff62aca0d8167fb9b728954f8ab40e94db7a469c.

--- funciones.js
+++ funciones.js
@@ -1,31 +1,38 @@
 function mostrarMapa()
-  {
-  let opcionesMapa = {
-    center:[10.230,-67.324],
-    zoom:15 
-  }
+{
+
+let opcionesMapa = {
+  center:[10.230,-67.324],
+  zoom:15
+}
 let objMapa = new L.map('divMapa' , opcionesMapa);
 
+/*
 let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 objMapa.addLayer(objCapa);
 
-let objMarca = new L.Marker([10.228,-67.323])
+let objMarca = new L.Marker([10.228,-67.3237])
 objMarca.addTo(objMapa);
+
+var objMapa = L.map('divMapa').setView([10.230,-67.324], 15);
+*/
+
+var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(objMapa);
+var marker = L.marker([10.228,-67.3237]).addTo(objMapa);
 
 }
 
 /*
-Solo LV:
-  center:[10.230,-67.324],
-  zoom:15 
-
-Desde LV hasta LT:
-  center:[10.235,-67.25],
-  zoom:13
-
+objMarca. 
 
 setInterval(function () {AlternarMarca()}, 1000);	// Actualizar el mapa cada pocos segundos
 
+*/
+
+/*
 variable opacidad
 cada 500mS
   asignar opacidad = abs(opacidad - 1)
