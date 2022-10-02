71511e9672ba1845821fe69e2ec7dc904bfe7a11
FedericoD3 <fvduran@gmail.com>
2022-10-02 14:51:49 -0400

    2022-10-02 14.51

--- funciones.js
+++ funciones.js
@@ -1,16 +1,9 @@
 function mostrarMapa()
 {
 let opcionesMapa = {
-  center:[10.230,-67.324],
-  zoom:15
-}
-
-/*
-let opcionesMapa = {
   center:[10.23,-67.250],
   zoom:13
 }
-*/
 let objMapa = new L.map('divMapa' , opcionesMapa);
 
 let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
@@ -20,6 +13,15 @@ let objMarca = new L.Marker([10.228,-67.3237])
 objMarca.addTo(objMapa);
 
 /*
+Solo La Victoria:
+let opcionesMapa = {
+  center:[10.230,-67.324],
+  zoom:15
+
+La Victoria hasta Tejerias
+let opcionesMapa = {
+  center:[10.23,-67.250],
+  zoom:13
 
 setInterval(function () {ActualizarMarca()}, 1000);	// Actualizar el mapa cada pocos segundos
 
