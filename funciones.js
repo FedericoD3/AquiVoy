8eac6a1f657c68aa9c8b484b2fa62dd7a5efc8e3
FedericoD3 <fvduran@gmail.com>
2022-10-02 15:03:46 -0400

    2022-10-02 15.03

--- funciones.js
+++ funciones.js
@@ -1,35 +1,18 @@
 function mostrarMapa()
 {
-let opcionesMapa = {
-  center:[10.23,-67.250],
-  zoom:13
-}
-let objMapa = new L.map('divMapa' , opcionesMapa);
 
-let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-objMapa.addLayer(objCapa);
+/*  let opcionesMapa = 
+    {
+    center:[10.23,-67.250],
+    zoom:13
+    }
+  let objMapa = new L.map('divMapa' , opcionesMapa);  */
+  var objMapa = L.map('divMapa').setView([10.230,-67.25], 13);
 
-let objMarca = new L.Marker([10.228,-67.3237])
-objMarca.addTo(objMapa);
+  let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+  objMapa.addLayer(objCapa);
 
-/*
-Solo La Victoria:
-let opcionesMapa = {
-  center:[10.230,-67.324],
-  zoom:15
+  let objMarca = new L.Marker([10.228,-67.3237])
+  objMarca.addTo(objMapa);
 
-La Victoria hasta Tejerias
-let opcionesMapa = {
-  center:[10.23,-67.250],
-  zoom:13
-
-setInterval(function () {ActualizarMarca()}, 1000);	// Actualizar el mapa cada pocos segundos
-
-variable opacidad
-cada 500mS
-  asignar opacidad = abs(opacidad - 1)
-  objMarca.setOpacity(opacidad)
-  Leer coordenadas nuevas de dweet.io
-  objMarca.setLatLng([lat,lon])
-*/
 }
