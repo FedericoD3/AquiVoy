c3ae6b3dd6c1625a3c257416744b2ce43569392b
FedericoD3 <fvduran@gmail.com>
2022-10-02 13:26:39 -0400

    2022-10-02 13.26

--- funciones.js
+++ funciones.js
@@ -1,42 +1,21 @@
 function mostrarMapa()
-/*
 {
+
 let opcionesMapa = {
   center:[10.230,-67.324],
   zoom:15
 }
 let objMapa = new L.map('divMapa' , opcionesMapa);
-*/
-/*
-var objMapa = L.map('divMapa').setView([10.230,-67.324], 15); 
 
 let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 objMapa.addLayer(objCapa);
 
 let objMarca = new L.Marker([10.228,-67.3237])
 objMarca.addTo(objMapa);
-*/
-
-
-
-var objMapa = L.map('divMapa').setView([10.230,-67.324], 15);
-var objCapa = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(objMapa);
-var objMarca = L.marker([10.228,-67.3237]).addTo(objMapa);
-
-/*
-var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(objMapa);
-var marker = L.marker([10.228,-67.3237]).addTo(objMapa);
-*/
 
 /*
 
-setInterval(function () {AlternarMarca()}, 1000);	// Actualizar el mapa cada pocos segundos
+setInterval(function () {ActualizarMarca()}, 1000);	// Actualizar el mapa cada pocos segundos
 
 variable opacidad
 cada 500mS
