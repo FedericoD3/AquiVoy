c07f393211c4ec3362e7729633ca005229584027
FedericoD3 <fvduran@gmail.com>
2022-10-01 20:11:36 -0400

    rehacer ordenamiento del JS

--- funciones.js
+++ funciones.js
@@ -7,10 +7,10 @@ let opcionesMapa = {
 }
 let objMapa = new L.map('divMapa' , opcionesMapa);
 
-/*
 let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 objMapa.addLayer(objCapa);
 
+/*
 let objMarca = new L.Marker([10.228,-67.3237])
 objMarca.addTo(objMapa);
 
