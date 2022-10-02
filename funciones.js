dc4cfea9b3583d038442c3b10744488bc83d2168
FedericoD3 <fvduran@gmail.com>
2022-10-02 15:55:33 -0400

    2022-10-02 15.55 var va sin new, let va con new

--- funciones.js
+++ funciones.js
@@ -1,11 +1,10 @@
 function mostrarMapa()
 {
-  var objMapa = new L.map('divMapa').setView([10.235,-67.24], 13);
+  var objMapa = L.map('divMapa').setView([10.235,-67.24], 13);
 
-  let objCapa = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-  objMapa.addLayer(objCapa);
+  var objCapa = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+  objMapa.addLayer(objCapa);  
 
   let objMarca = new L.Marker([10.228,-67.3237])
   objMarca.addTo(objMapa);
-
 }
