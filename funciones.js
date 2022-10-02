f0f6f84fcdd6e8108a28e92057ba3721c6a50d15
FedericoD3 <fvduran@gmail.com>
2022-10-02 15:51:52 -0400

    2022-10-02 15.51 Corrección TileLayer -> tileLayer

--- funciones.js
+++ funciones.js
@@ -1,15 +1,8 @@
 function mostrarMapa()
 {
+  var objMapa = new L.map('divMapa').setView([10.235,-67.24], 13);
 
-/*  let opcionesMapa = 
-    {
-    center:[10.23,-67.250],
-    zoom:13
-    }
-  let objMapa = new L.map('divMapa' , opcionesMapa);  */
-  var objMapa = new L.map('divMapa').setView([10.230,-67.25], 13);
-
-  let objCapa = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+  let objCapa = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
   objMapa.addLayer(objCapa);
 
   let objMarca = new L.Marker([10.228,-67.3237])
