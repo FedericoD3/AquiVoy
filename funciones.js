4ad09ce36c760fef4b4573475e245a906aa7b3fa
FedericoD3 <fvduran@gmail.com>
2022-10-03 17:41:55 -0400

    2022-10-03 17.41 Con poligonos de lugares

--- funciones.js
+++ funciones.js
@@ -8,7 +8,22 @@ function mostrarMapa()
       
   var objMarca = L.marker([10.230,-67.320]).addTo(objMapa);
 
-  var sitioCilento = L.polygon([
+
+  var lugar_Unicentro =L.polygon([
+    [10.2264, -67.3323],
+    [10.2263, -67.3334],
+    [10.2266, -67.3334],
+    [10.2267, -67.3323]
+  ])
+
+  var lugar_LiceoRibas = L.polygon([
+    [10.2263, -67.3300],
+    [10.2254, -67.3298],
+    [10.2253, -67.3309],
+    [10.2262, -67.3310]
+  ])
+
+  var lugar_Cilento = L.polygon([
     [10.230,-67.322],
     [10.228,-67.322],
     [10.226,-67.325],
