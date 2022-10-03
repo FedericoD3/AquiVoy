a1f180c5a818ec6a919792f5a0b13f8ae226e078
FedericoD3 <fvduran@gmail.com>
2022-10-03 18:04:04 -0400

    2022-10-03 18.03 Poligonos a 0,1 micro grados (aprox 10cm)

--- funciones.js
+++ funciones.js
@@ -8,12 +8,17 @@ function mostrarMapa()
       
   var objMarca = L.marker([10.230,-67.320]).addTo(objMapa);
 
+/* 
+  Perimetro terrestre = 40.030.173m 
+  1° = 111.196m
+  1m = 9µ° 
+*/
 
   var lugar_Unicentro =L.polygon([
-    [10.2264, -67.3323],
-    [10.2263, -67.3334],
-    [10.2266, -67.3334],
-    [10.2267, -67.3323]
+    [10.2263510, -67.3322838],
+    [10.2263062, -67.3334009],
+    [10.2267114, -67.3334599],
+    [10.2268302, -67.3322918]
   ]).addTo(objMapa);
 
   var lugar_LiceoRibas = L.polygon([
@@ -28,6 +33,13 @@ function mostrarMapa()
     [10.2277, -67.3235],
     [10.2272, -67.3232],
     [10.2266, -67.3241]
+  ]).addTo(objMapa);
+
+  var lugar_Solidario = L.polygon([
+    [10.2290, -67.3211],
+    [10.2282, -67.3207],
+    [10.2280, -67.3212],
+    [10.2289, -67.3216]
   ]).addTo(objMapa);
 }
 
