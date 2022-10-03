a0eb1d991ff298c04d381ae01157d51cf0a8b902
FedericoD3 <fvduran@gmail.com>
2022-10-02 20:44:14 -0400

    2022-10-02 20.44 con un poligono

--- funciones.js
+++ funciones.js
@@ -5,6 +5,13 @@ function mostrarMapa()
   var objCapa = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
       objMapa.addLayer(objCapa); 
       
-  var objMarca = L.marker([10.230,-67.322]).addTo(objMapa);
+  var objMarca = L.marker([10.230,-67.320]).addTo(objMapa);
+
+  var sitioCilento = L.polygon([
+    [10.230,-67.322],
+    [10.228,-67.322],
+    [10.226,-67.325],
+    [10.224,-67.327]
+  ]).addTo(map);
 }
 
