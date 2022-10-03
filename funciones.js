c1de899e2208e56ac22bd92e8e1b3436e4260c93
FedericoD3 <fvduran@gmail.com>
2022-10-02 20:24:16 -0400

    2022-10-02 20.24

--- funciones.js
+++ funciones.js
@@ -3,9 +3,8 @@ function mostrarMapa()
   var objMapa = L.map('divMapa').setView([10.235,-67.24], 13);
 
   var objCapa = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-  objMapa.addLayer(objCapa);
-
-  let objMarca = new L.Marker([10.228,-67.3237])
-  objMarca.addTo(objMapa);
+      objMapa.addLayer(objCapa); 
+      
+  var objMarca = L.marker([10.230,-67.322]).addTo(objMapa);
 }
 
